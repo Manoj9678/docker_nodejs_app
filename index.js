c28fe54e72dcf965ab61405792e43b2c9c6e62c2
Manoj9678 <141323636+Manoj9678@users.noreply.github.com>
2024-05-31 20:05:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const PORT = 3000;
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World
+           good morining i am Manoj
+    i am came from in your heart');
 });
 
 app.listen(PORT, () => {
